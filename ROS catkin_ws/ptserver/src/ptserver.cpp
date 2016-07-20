ac318781c9be02768301a40a5d2b1646e394e66d
Photon <1487quantum@users.noreply.github.com>
2016-07-20 14:46:44 +0800

    Published extracted points
    - Advertise as /target_pts
    - Refactored code

--- ROS catkin_ws/ptserver/src/ptserver.cpp
+++ ROS catkin_ws/ptserver/src/ptserver.cpp
@@ -3,6 +3,9 @@
 #include <rosbag/view.h>
 #include <geometry_msgs/PoseStamped.h>
 
+#include "std_msgs/String.h"
+#include <sstream>
+
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
 
@@ -22,9 +25,34 @@ int count;
 vec_d coordX,coordY, angW;
 std::string const& fpath = "/home/fyp-trolley/catkin_ws/waypts.bag";  //Bag location
 
+std_msgs::String msg;
+std::stringstream ss;
+
+class ptServer{
+public:
+  ptServer();
+  void countContents(std::string const& filename);
+  void dumpContents(rosbag::Bag& b);
+  void findBag(std::string const& filename);
+  void p2p();
+
+private:
+  void publishPoint(std_msgs::String msg);
+  ros::Publisher pointPub; // publish robot_pose
+  ros::NodeHandle nh; // Nodehandler
+  ros::Timer timeout; // Ros timer
+};
+
+ptServer::ptServer(){
+  pointPub= nh.advertise<std_msgs::String>("/target_pts", 10);
+}
+
+void ptServer::publishPoint(std_msgs::String msg){
+  pointPub.publish(msg);
+}
 
 //Count no of waypoints
-void countContents(std::string const& filename) {
+void ptServer::countContents(std::string const& filename) {
   ROS_INFO_STREAM("Counting presents..");
 
   rosbag::Bag b;
@@ -46,26 +74,35 @@ void countContents(std::string const& filename) {
 }
 
 //Extract contents
-void dumpContents(rosbag::Bag& b) {
+void ptServer::dumpContents(rosbag::Bag& b) {
   ROS_INFO_STREAM("Opening presents..");
   count = 0;
   rosbag::View view(b);
+
   foreach(rosbag::MessageInstance m, view){
     geometry_msgs::PoseStamped::ConstPtr msg_g = m.instantiate<geometry_msgs::PoseStamped>();
     coordX.push_back(msg_g->pose.position.x);
     coordY.push_back(msg_g->pose.position.y);
     angW.push_back(msg_g->pose.orientation.w);
 
+    //Display in console
     std::cout << "Waypoint " << count << ":"<< std::endl; //Waypoint id
     std::cout << coordX.at(count) << ", "; //X
     std::cout << coordY.at(count) << ", "; //Y
     std::cout << angW.at(count) << std::endl; //Angle
+
+    //For publishing
+    ss << count << ", " << coordX.at(count) << ", " << coordY.at(count) << ", " << angW.at(count) << "\n";
+    msg.data = ss.str();
     count++;
   }
+  ROS_INFO("%s", msg.data.c_str());
+  publishPoint(msg);
+
 }
 
 //Open bag location
-void findBag(std::string const& filename) {
+void ptServer::findBag(std::string const& filename) {
   rosbag::Bag b;
   b.open(filename, rosbag::bagmode::Read);
   dumpContents(b);
@@ -73,7 +110,7 @@ void findBag(std::string const& filename) {
 }
 
 //Point to point navigation
-void p2p(){
+void ptServer::p2p(){
   //tell the action client that we want to spin a thread by default
   MoveBaseClient ac("move_base", true);
 
@@ -89,8 +126,9 @@ void p2p(){
   goal.target_pose.header.stamp = ros::Time::now();
 
   double distance_x = .5;
-  goal.target_pose.pose.position.x = 0.5;
-  goal.target_pose.pose.orientation.w = 0.45;
+  goal.target_pose.pose.position.x = 0.3;
+  goal.target_pose.pose.position.y = 1.0;
+  goal.target_pose.pose.orientation.w = 1;
 
   ROS_INFO("Sending goal");
   ac.sendGoal(goal);
@@ -105,12 +143,9 @@ void p2p(){
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "ptserver_node");
-
-  //rosbag::Bag bag;
-  //bag.open("/home/fyp-trolley/catkin_ws/waypts.bag", rosbag::bagmode::Read);
-
-  countContents(fpath);
-  findBag(fpath);
+  ptServer ps;
+  ps.countContents(fpath);
+  ps.findBag(fpath);
   //p2p();
   ros::spin();
   return 0;
