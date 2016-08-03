1887485170337d4f929c6373cf7085a9128170e1
Photon <1487quantum@users.noreply.github.com>
2016-08-03 16:18:01 +0800

    Improvements to p2p navigation
    -Subscribed to robot_pose_publisher
    -Subscribed to joy_node
    -Added new cmd_vel publisher , velPub
    -Added skeleton code for ebrake

--- ROS catkin_ws/ptserver/src/ptserver.cpp
+++ ROS catkin_ws/ptserver/src/ptserver.cpp
@@ -3,6 +3,10 @@
 #include <rosbag/view.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <tf/transform_broadcaster.h>
+#include <geometry_msgs/Pose.h>
+#include <math.h>
+#include <geometry_msgs/Twist.h>
+#include <sensor_msgs/Joy.h>
 
 #include "std_msgs/String.h"
 #include <sstream>
@@ -10,13 +14,15 @@
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
 
-typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
-
 #include <boost/foreach.hpp>
 #define foreach BOOST_FOREACH
 
 #include <vector>
+
 typedef std::vector<double> vec_d;  //Allows dynamic sizing
+typedef boost::shared_ptr<geometry_msgs::PoseStamped const> PoseConstPtr;
+typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+
 
 //For I/O
 #include <fstream>
@@ -25,10 +31,13 @@ typedef std::vector<double> vec_d;  //Allows dynamic sizing
 int count;
 vec_d coordX,coordY, angW,angZ;
 double currentX,currentY;
+double distToGoal,distToGoalprev;
 std::string const& fpath = "/home/fyp-trolley/catkin_ws/waypts.bag";  //Bag location
 
 geometry_msgs::PoseStamped msg;
-std::stringstream ss;
+geometry_msgs::PoseStamped pos; //current pos
+double xd,xy;
+int wayptCounter,eBrake;
 
 class ptServer{
 public:
@@ -38,23 +47,55 @@ public:
   void findBag(std::string const& filename);
   void p2p(double distance_x,double distance_y,double angle_z,double angle_w);
   void publishPoint(geometry_msgs::PoseStamped msg);
+  void poseCallback(const PoseConstPtr& msg);
+  void joyCallback(const sensor_msgs::Joy::ConstPtr &msg);
+  void publishVel(geometry_msgs::Twist msg);
+
+
 
 
 private:
-  ros::Publisher pointPub; // publish robot_pose
+  ros::Publisher pointPub; // publish pose_goal
+  ros::Publisher velPub; // publish cmd_vel
+  ros::Publisher distToGoalPub;
+  ros::Subscriber poseSub; // Subscribe to robot_pose
+  ros::Subscriber joySub; // subscribe to joystick
   ros::NodeHandle nh; // Nodehandler
   ros::Timer timeout; // Ros timer
 };
 
 ptServer::ptServer(){
+  velPub= nh.advertise<geometry_msgs::Twist>("cmd_vel", 1);
   pointPub= nh.advertise<geometry_msgs::PoseStamped>("/target_pts", 10);
-  //ros::Subscriber sub = nh.subscribe("odom", 1000, chatterCallback);
+  poseSub= nh.subscribe("/robot_pose", 10, &ptServer::poseCallback, this);
+  joySub = nh.subscribe("/joy", 10, &ptServer::joyCallback, this);
 }
 
 void ptServer::publishPoint(geometry_msgs::PoseStamped msg){
   pointPub.publish(msg);
 }
+void ptServer::publishVel(geometry_msgs::Twist msg){
+  velPub.publish(msg);
+}
 
+void ptServer::poseCallback(const PoseConstPtr& msg) {
+  pos = *msg;
+}
+void ptServer::joyCallback(const sensor_msgs::Joy::ConstPtr &msg) {
+  // process and publish
+  geometry_msgs::Twist twistMsg;
+
+  // check button, change variable button to switch to another button
+  bool switchActive = (msg->buttons[2] == 1);
+  if (switchActive) {
+      eBrake=1;
+    }
+  else
+  {
+    eBrake=0;
+  }
+
+}
 
 
 //Count no of waypoints
@@ -70,7 +111,7 @@ void ptServer::countContents(std::string const& filename) {
   foreach(rosbag::MessageInstance m, view) {
     geometry_msgs::PoseStamped::ConstPtr s = m.instantiate<geometry_msgs::PoseStamped>(); //Count only geometry_msgs::PoseStamped type
     if (s != NULL) {
-      //ASSERT_EQ(s->data, foo_.data);
+
       message_count++;
     }
   }
@@ -130,7 +171,7 @@ void ptServer::p2p(double distance_x,double distance_y,double angle_z,double ang
   move_base_msgs::MoveBaseGoal goal;
   geometry_msgs::PoseStamped goalx;
 
-  //we'll send a goal to the robot to move 1 meter forward
+  //we'll send a goal to the robot to move to the waypoints
   goal.target_pose.header.frame_id = "map"; // reference to map
   goal.target_pose.header.stamp = ros::Time::now();
 
@@ -155,29 +196,55 @@ void ptServer::p2p(double distance_x,double distance_y,double angle_z,double ang
   geometry_msgs::Quaternion qMsg;
   tf::quaternionTFToMsg(quaternion, qMsg);
 
-  //goal.target_pose.pose.orientation = qMsg;
-    goal.target_pose.pose.orientation.x = 0.0;
-    goal.target_pose.pose.orientation.y = 0.0;
-    goal.target_pose.pose.orientation.z = angle_z;
-    goal.target_pose.pose.orientation.w = angle_w;
+  // Set target goal
+  goal.target_pose.pose.orientation.x = 0.0;
+  goal.target_pose.pose.orientation.y = 0.0;
+  goal.target_pose.pose.orientation.z = angle_z;
+  goal.target_pose.pose.orientation.w = angle_w;
 
+  // Set goal marker
+  goalx.pose.orientation.x = 0.0;
+  goalx.pose.orientation.y = 0.0;
+  goalx.pose.orientation.z = angle_z;
+  goalx.pose.orientation.w = angle_w;
 
-    goalx.pose.orientation.x = 0.0;
-    goalx.pose.orientation.y = 0.0;
-    goalx.pose.orientation.z = angle_z;
-    goalx.pose.orientation.w = angle_w;
-
-  ROS_INFO("Sending next command");
+  ROS_INFO("Sending next goal");
   ac.sendGoal(goal);
   publishPoint(goalx);
+  wayptCounter++;
 
+  double xd=goalx.pose.position.x-pos.pose.position.x;
+  double yd=goalx.pose.position.y-pos.pose.position.y;
+  double odz=abs(goalx.pose.orientation.z-pos.pose.orientation.z);
+  distToGoal=sqrt(xd*xd+yd*yd);
+
+  if ((wayptCounter%10)!=0)
+  {
+    while (distToGoal>0.5 || (odz>0.25))
+    {
+
+      xd=(distance_x)-pos.pose.position.x;
+      yd=(distance_y)-pos.pose.position.y;
+      odz=abs(goalx.pose.orientation.z-pos.pose.orientation.z);
+      distToGoal=sqrt(xd*xd+yd*yd);
+
+      ros::spinOnce();
+    }
+  }
+  else
+  {
+    ac.waitForResult();
+  }
+
+  ROS_INFO("The base is approaching x:%f y:%f facing z:%f w:%f ", distance_x,distance_y,angle_z,angle_w);
+  /*
   ac.waitForResult();
   ROS_INFO("Moving to goal");
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
   ROS_INFO("The base successfully moved to %f,%f facing %f,%f ", distance_x,distance_y,angle_z,angle_w);
   else
   ROS_INFO("The base failed to move to %f,%f facing %f,%f ", distance_x,distance_y,angle_z,angle_w);
-
+  */
 }
 
 
@@ -189,14 +256,10 @@ int main(int argc, char** argv){
   ps.countContents(fpath);
   ps.findBag(fpath);
 
-  //msg.data = ss.str();
-  //ps.publishPoint(msg);
-
   for (double i=0.0; i<coordX.size(); i+=1)
   {
     ps.p2p(coordX.at(i),coordY.at(i),angZ.at(i),angW.at(i)); // send goals
-    //ROS_INFO("Waiting for 2sec...");
-    //ros::Duration(2).sleep(); // wait 2 sec
+
   }
 
 
